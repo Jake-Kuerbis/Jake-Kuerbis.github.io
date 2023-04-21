a1318404270d03f4c3b9b10c17c634866e1ba99d
Jake-Kuerbis <kuerj524@newschool.edu>
2023-04-21 00:17:08 -0400

    yes

--- project3/assets/data.js
+++ project3/assets/data.js
@@ -1,15 +1,35 @@
-const dataUrl = 'https://data.cityofnewyork.us/resource/bkwf-xfky.json'
+$(document).ready(function () {
 
-const parseData = (classes) => {
+    $('body').css('display', 'none');
+    $('body').fadeIn(2000);
+
+});
+
+
+console.log('---------------- NEW STUFF ------------------')
+
+const dataUrl = 'https://data.cityofnewyork.us/resource/bkwf-xfky.json'
+const graph = document.querySelector('#graph')
+const main = document.querySelector('main')
+
+let sampleData = []
+
+const parseData = (data) => {
     let sampleCompliance = 0
     let sampleOperational = 0
     let sampleResample = 0
+    let turbidity = 0
 
-    classes.forEach(classs => {
-        if (classs.sample_class == 'Compliance') {
+    data.forEach(sample => {
+        const samplePoint = document.createElement('div')
+        samplePoint.classList.add('sampleData')
+        main.appendChild(samplePoint)
+
+        if (sample.sample_class == 'Compliance') {
             sampleCompliance++
+            
         } 
-        else if (classs.sample_class == 'Operational') {
+        else if (sample.sample_class == 'Operational') {
             sampleOperational++
         }
         else {
@@ -23,10 +43,18 @@ const parseData = (classes) => {
     console.log('compliance sample: ' + sampleCompliance)
     console.log('operational sample: ' + sampleOperational)
     console.log('resample: ' + sampleResample)
+
+    document.getElementById('gray').innerHTML = 'Compliance Sample: ' + sampleCompliance;
+    document.getElementById('orange').innerHTML = 'Operational sample: ' + sampleOperational;
+    document.getElementById('red').innerHTML = 'Resample: ' + sampleResample;
+
+    graph.style.setProperty('--color--gray', sampleCompliance)
+    graph.style.setProperty('--color--orange', sampleOperational)
+    graph.style.setProperty('--color--red', sampleResample)
 }
 
 
-fetch(dataUrl + '?$limit=100000')
+fetch(dataUrl + '?$limit=200000')
     .then(response => response.json())
     .then(data => {
         parseData(data)
@@ -36,3 +64,4 @@ fetch(dataUrl + '?$limit=100000')
 //&sample_class=Compliance
 //&turbidity_ntu=0.84
 //$where=turbidity_ntu > 1
+
