10fe397c692be401238d4e829e98e3c3811e0b43
Jake-Kuerbis <kuerj524@newschool.edu>
2023-05-05 09:16:53 -0400

    images

--- project3/assets/data.js
+++ project3/assets/data.js
@@ -8,56 +8,86 @@ $(document).ready(function () {
 
 */
 
+(function() {
+    var pres = document.querySelectorAll("#background_vidd img");
+    var len = pres.length;
+    for(var i = 0; i < pres.length; i = i + 1) {
+        pres[i].style.display = 'none';
+    }
+    var a = 0;
+    window.setInterval(function() {
+        pres[a].style.display = 'none';
+        pres[(a + 1)%len].style.display = 'block';
+        a = (a + 1) % len;
+    }, 800);
+}());
+
+
+
 
 console.log('---------------- NEW STUFF ------------------')
 
-const dataUrl = 'https://data.cityofnewyork.us/resource/bkwf-xfky.json'
+const dataUrl = 'https://data.cityofnewyork.us/resource/2xir-kwzz.json'
 const graph = document.querySelector('#graph')
 const main = document.querySelector('main')
 
 let sampleData = []
 
 const parseData = (data) => {
-    let sampleCompliance = 0
-    let sampleOperational = 0
-    let sampleResample = 0
+    let sampleID = 0
+    let beachName = 0
+    let enterococci = 0
+    let sampleLocation = 0
     let turbidity = 0
+    let date = '092022'
+
 
     data.forEach(sample => {
         const samplePoint = document.createElement('div')
         samplePoint.classList.add('sampleData')
         main.appendChild(samplePoint)
 
-        if (sample.sample_class == 'Compliance') {
-            sampleCompliance++
-            
+        const listItemTwo = document.createElement('li') // Make the `li`
+        
+        const itemTitleThree = document.querySelector('.p') // Make an `h2`
+		itemTitleThree.innerHTML = date// Put the JSON title inside
+		listItemTwo.appendChild(itemTitleThree) // And add it to the `li`!
+
+        const itemTitleTwo = document.createElement('p') // Make an `h2`
+		itemTitleTwo.innerHTML = sample.beach_name + '<br>' + '<br>' + sample.sample_id + '<br>' + 'sample location: ' + sample.sample_location + '<br>' + 'enterococci results: ' + sample.enterococci_results// Put the JSON title inside
+		listItemTwo.appendChild(itemTitleTwo) // And add it to the `li`!
+
+        
+
+        if (sample.beach_name = 'beach_name', sample.sample_id = 'sample_id',  sample.enterococci_results = 'enterococci_results' && sample.sample_location == 'Center') {
+            beachName++
+            sampleID++
+            sampleLocation++
+            enterococci++
         } 
-        else if (sample.sample_class == 'Operational') {
-            sampleOperational++
-        }
-        else {
-            sampleResample++
-        }
+        
+    
 
        // console.log(classs.sample_class)
+       graph.appendChild(listItemTwo)
 
     })
 
-    console.log('compliance sample: ' + sampleCompliance)
-    console.log('operational sample: ' + sampleOperational)
-    console.log('resample: ' + sampleResample)
+    console.log('beach name: ' + beachName)
+    console.log('sample id: ' + sampleID)
+    console.log('sample location: ' + sampleLocation)
 
-    document.getElementById('gray').innerHTML = 'Compliance Sample: ' + sampleCompliance;
-    document.getElementById('orange').innerHTML = 'Operational sample: ' + sampleOperational;
-    document.getElementById('red').innerHTML = 'Resample: ' + sampleResample;
+    document.getElementById('gray').innerHTML = 'Beach Name: ' + beachName;
+    document.getElementById('orange').innerHTML = 'Sample ID: ' + sampleID;
+    document.getElementById('red').innerHTML = 'Sample Location: ' + sampleLocation;
 
-    graph.style.setProperty('--color--gray', sampleCompliance)
-    graph.style.setProperty('--color--orange', sampleOperational)
-    graph.style.setProperty('--color--red', sampleResample)
+    graph.style.setProperty('--color--gray', beachName)
+    graph.style.setProperty('--color--orange', sampleID)
+    graph.style.setProperty('--color--red', sampleLocation)
 }
 
 
-fetch(dataUrl + '?$limit=200000')
+fetch(dataUrl + '?$limit=150')
     .then(response => response.json())
     .then(data => {
         parseData(data)
@@ -68,3 +98,29 @@ fetch(dataUrl + '?$limit=200000')
 //&turbidity_ntu=0.84
 //$where=turbidity_ntu > 1
 
+function myFunction() {
+    var click = document.querySelector(".containerTwo");
+    var clickResize = document.querySelector(".ManhattanBeachClick");
+    var disappear = document.querySelector('.hoverContainer');
+    var disappearr = document.querySelector('.hoverContainerClick')
+    var clickResizeTwo = document.querySelector(".ManhattanBeachHoverClick");
+    var clickResizeThree = document.querySelector(".hoverContainerTwo");
+    var offClick = document.querySelector(".containerTwo");
+    var display = document.querySelector("#background_vid");
+    if (disappear.style.display === "block") {
+        click.style.gridTemplateColumns = "repeat(3, 1fr)";
+        disappear.style.display = "none";
+        disappearr.style.display = "block";
+        //clickResize.style.width = "35rem";
+        //clickResizeTwo.style.width = "35rem";
+        //clickResizeThree.style.width = "22rem";
+        display.style.display = "block";
+      } else {
+        disappear.style.display = "block";
+        disappearr.style.display = "none";
+        //click.style.gridTemplateColumns = "repeat(3, 1fr)";
+        //clickResize.style.width = "12rem";
+        //clickResizeTwo.style.width = "12rem";
+        display.style.display = "none";
+      }
+}
